ef0b05d47287a8ef844f981ad5d85dac63c30cde
alexandredess <alexandre.dessoly@laposte.net>
2021-01-12 13:17:55 +0100

    mod register controller

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -11,10 +12,9 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="home")
      */
-    public function index(): Response
+    public function index(Request $request)
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
-        ]);
+        return $this->render('home/index.html.twig');
+        
     }
 }
--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -6,6 +6,7 @@ use App\Entity\User;
 use App\Form\RegisterType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class RegisterController extends AbstractController
@@ -13,11 +14,19 @@ class RegisterController extends AbstractController
     /**
      * @Route("/inscription", name="register")
      */
-    public function index(): Response
+    public function index(Request $request)
     {
         $user = new User();
-        $form = $this->createForm(RegisterType::class, $user);
 
+        $form = $this->createForm(RegisterType::class,$user);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()){
+            $user = $form->getData();
+
+            
+        }
 
         return $this->render('register/index.html.twig',[
             'form'=>$form->createView()
