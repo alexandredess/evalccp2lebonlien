7f97114a33cc76f08a82645f0cb3ae210d78897e
alexandredess <alexandre.dessoly@laposte.net>
2021-01-18 08:50:55 +0100

    wysiwyg cgu

--- src/Controller/Admin/CguCrudController.php
+++ src/Controller/Admin/CguCrudController.php
@@ -4,6 +4,9 @@ namespace App\Controller\Admin;
 
 use App\Entity\Cgu;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\CodeEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class CguCrudController extends AbstractCrudController
 {
@@ -12,14 +15,13 @@ class CguCrudController extends AbstractCrudController
         return Cgu::class;
     }
 
-    /*
+    
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
+            TextField::new('titre'),
             TextEditorField::new('description'),
         ];
     }
-    */
+    
 }
