cf5aa20afe8b15fc2e37489320a471e00acceb6d
alexandredess <alexandre.dessoly@laposte.net>
2021-01-24 08:36:46 +0100

    crud header, entity header, il faut faire l'affichage dans la page d'accueil

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -9,6 +9,7 @@ use App\Entity\Cgu;
 use App\Entity\Cgv;
 use App\Entity\Contact;
 use App\Entity\Genre;
+use App\Entity\Header;
 use App\Entity\Product;
 use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
@@ -41,6 +42,7 @@ class DashboardController extends AbstractDashboardController
         yield MenuItem::linkToCrud('Genre', 'fas fa-list', Genre::class);
         yield MenuItem::linkToCrud('Transporteur', 'fas fa-truck',Carrier::class);
         yield MenuItem::linkToCrud('Page accueil', 'fas fa-pager', Accueil::class);
+        yield MenuItem::linkToCrud('header', 'fas fa-file-image', Header::class);
         yield MenuItem::linkToCrud('Produits', 'fas fa-tag', Product::class);
         yield MenuItem::linkToCrud('Contact', 'fas fa-file-contract', Contact::class);
         yield MenuItem::linkToCrud('Cgu', 'fas fa-list', Cgu::class);
---
+++ src/Controller/Admin/HeaderCrudController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Header;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+
+class HeaderCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Header::class;
+    }
+
+    
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            TextField::new('title','Tire de la bannière'),
+            TextareaField::new('content','Description'),
+            TextField::new('btn_title','Titre de notre bouton'),
+            TextField::new('btn_url','url de destination du bouton'),
+            ImageField::new('illustration')
+                ->setBasePath('uploads/')
+                ->setUploadDir('public/uploads')
+                ->setUploadedFileNamePattern('[randomhash].[extension]')
+                ->setRequired(false),
+        ];
+    }
+    
+}
